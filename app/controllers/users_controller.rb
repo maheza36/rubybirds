6f69b5b4fb9c8be2690952f7d2a8dd8036fbd3a3
Mauricio Hernández <maheza36@gmail.com>
2016-11-08 21:56:19 -0500

    ajustes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 include SessionsHelper
 
 
-  def new
+  def new #carga la vista
     #@user = User.new(first_name: "Jorgetas", last_name: "Gomez")
     #@user = User.new()
     #User.create
@@ -13,14 +13,15 @@ include SessionsHelper
   end
 
   def create
-    @user = User.new(sessions_params)
+    @user = User.new(user_params)
     if @user.save
+      login user
       redirect_to root_path
     end
   end
 
 end
 private
-def sessions_params
+def user_params
   params.require(:user).permit(:nickname, :first_name, :last_name, :email, :password, :password_confirmation)
 end
